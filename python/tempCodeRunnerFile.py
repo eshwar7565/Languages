9b03255c5be4ecb9ce55d357f6e32d3204ca018d
Ëshwar <eshwarrachakonda02@gmail.com>
2022-09-18 10:43:02 +0530

    quick sort

---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+
+ # Recursive call on the left of pivot
+ quick_sort(array, low, pi - 1)
+
+ # Recursive call on the right of pivot
+ quick_sort(array, pi + 1, high)
+
+
+  
+# Driver code
+array = [ 1, 7, 8, 9, 1, 2]
